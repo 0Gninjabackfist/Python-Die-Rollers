93e1bbea2f5b18a88be24ab2e76093ffab92e239
0Gninjabackfist <58711690+0Gninjabackfist@users.noreply.github.com>
2019-12-23 16:10:58 -0800

    commit

---
+++ 03 Dice custom function.py
@@ -0,0 +1,13 @@
+import random  #import the random library to use randint
+
+def gdie():  #define the D6 function as gdie (gamma die)
+    dieroll = random.randint(1, 6) #dieroll = a random integer from 1-6 using randint from the random libray
+    if dieroll == 1: dieroll = 1
+    if dieroll == 2: dieroll = 1
+    if dieroll == 3: dieroll = 2
+    if dieroll == 4: dieroll = 0
+    if dieroll == 5: dieroll = 0
+    if dieroll == 6: dieroll = 0
+    #return dieroll
+    print(dieroll)
+gdie()
