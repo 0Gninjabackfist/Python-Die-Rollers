9e9d8c95a4ad0b5e9985d61e06a31c4c658f962a
0Gninjabackfist <58711690+0Gninjabackfist@users.noreply.github.com>
2020-01-06 14:28:46 -0800

    function with lists

--- 09 Lists.py
+++ 09 Lists.py
@@ -2,50 +2,19 @@ import random  #import the random library to use randint
 
 def roll(die):  #define the D6 function as gdie (gamma die)
     intel = 0 #setting intel to 0 for all dice so it can be assigned to 1 if the die result is 0
+    roll = random.randint(1, 6) #roll = a random integer from 1-6 using randint from the random library
+    gdie = [1,1,2,0,0,0]
+    bdie = [1,1,2,3,0,0]
+    adie = [1,2,3,4,5,0]
+    odie = [1,1,0,0,0,0]
     if die == 'g':
-        groll = random.randint(1, 6) #groll = a random integer from 1-6 using randint from the random library
-        if groll == 1: groll = 1
-        if groll == 2: groll = 1
-        if groll == 3: groll = 2
-        if groll == 4: groll = 0
-        if groll == 5: groll = 0
-        if groll == 6: groll = 0
+       
+
         if groll == 0: intel = 1 #this assignes intel to 1 if the die rolls a 0, it's default a 0 itself
         return (groll, intel) #<<how is this going to work?
-    if die == 'b':
-        broll = random.randint(1, 6) #broll = a random integer from 1-6 using randint from the random library
-        if broll == 1: broll = 1
-        if broll == 2: broll = 1
-        if broll == 3: broll = 2
-        if broll == 4: broll = 3
-        if broll == 5: broll = 0
-        if broll == 6: broll = 0
-        if broll == 0: intel = 1 
-        return (broll, intel) 
-        
-    if die == 'a':
-        aroll = random.randint(1, 6) #aroll = a random integer from 1-6 using randint from the random library
-        if aroll == 1: aroll = 1
-        if aroll == 2: aroll = 2
-        if aroll == 3: aroll = 3
-        if aroll == 4: aroll = 4
-        if aroll == 5: aroll = 5
-        if aroll == 6: aroll = 0
-        if aroll == 0: intel = 1 
-        return (aroll, intel) 
-    if die == 'o':
-        oroll = random.randint(1, 6) #oroll = a random integer from 1-6 using randint from the random library
-        if oroll == 1: oroll = 1
-        if oroll == 2: oroll = 1
-        if oroll == 3: oroll = 0
-        if oroll == 4: oroll = 0
-        if oroll == 5: oroll = 0
-        if oroll == 6: oroll = 0
-        return (oroll, intel) #set oroll to the number rolled and assign intel to 1 if roll is 0
-gdie = [1,1,2,0,0,0]
-bdie = [1,1,2,3,0,0]
-adie = [1,2,3,4,5,0]
-odie = [1,1,0,0,0,0]
+
+
+
 
 (groll, gintel) = roll('g') #these names don't have to match the variables from 'g' die function
 (broll, bintel) = roll('b') #these names don't have to match the variables from 'b' die function
