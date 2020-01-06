5ae57ddf74b8e65b70d328fac0cac4bb3b43f17f
0Gninjabackfist <58711690+0Gninjabackfist@users.noreply.github.com>
2020-01-06 15:39:38 -0800

    roll function complete

--- 10 Dictionary.py
+++ 10 Dictionary.py
@@ -2,7 +2,7 @@ import random  #import the random library to use randint
 
 def roll(die):  #define die rolling function
     intel = 0 #setting intel to 0 for all dice so it can be assigned to 1 if the die result is 0
-    roll = random.randint(0, 5) #roll = a random integer from 1-6 using randint from the random library
+    face = random.randint(0, 5) #roll = a random integer from 1-6 using randint from the random library
     
     dice = { #this is a dictionary the index comes from die, and the value is counted based on the roll value
         "g": [1,1,2,0,0,0], 
@@ -10,16 +10,17 @@ def roll(die):  #define die rolling function
         "a": [1,2,3,4,5,0],
         "o": [1,1,0,0,0,0]
     }
+    value = dice[die][face] # creates a variable that returns the value of the die of that type on that face
+    if value == 0: intel = 1 #this sets to the Intel to 1 if a blank was rolled
+    return (value, intel, face) 
+    #the face here is if later i need to know which side of the die was roll 0-5
 
-    if dice[die][roll] == 0: intel = 1 #this calld dice
-    return (dice[die][roll], intel)
 
-
-(groll, gintel) = roll('g') #these names don't have to match the variables from 'g' die function
-(broll, bintel) = roll('b') #these names don't have to match the variables from 'b' die function
-(aroll, aintel) = roll('a') #these names don't have to match the variables from 'a' die function
-(oroll, ointel) = roll('o') #these names don't have to match the variables from 'o' die function
-#these variables are only for use outside of the function and are populated by the tuple in the function when called
+(groll, gintel, _) = roll('g') #these names don't have to match the variables from 'g' die function, the underscore is telling it to ignore
+(broll, bintel, _) = roll('b') 
+(aroll, aintel, _) = roll('a') 
+(oroll, ointel, _) = roll('o')
+#these variables are only for use outside of the function and are populated by the values in the function when called
 
 
 print("Alpha die result is", aroll, "and there was", aintel, "Intelligence generated")
